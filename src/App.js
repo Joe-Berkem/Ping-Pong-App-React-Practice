3903496862adba5dad0a2db4aad646c5971e6a6f
Joe Berkem <jberkem@gmail.com>
2019-03-18 16:36:47 +0000

    state updating and serving appears correctly

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = ({
+  player1, 
+  player2, 
+  player1Serving,
+  handlePlayer1Increment, 
+  handlePlayer2Increment,
+  handleReset
+}) => (
 
+  <React.Fragment>
+      {/* header */}
+      <header className="page-header">
+          <h1>PongPing</h1>
+      </header>
+
+      {/* scores */}
+      <div className="row">
+          <div className="col-xs-6">
+              <p>Player 1 {player1Serving &&
+                 <span className="pull-right label label-success">Serving</span>}
+              </p> 
+              
+              <p className="well">{ player1 }</p>
+              <button 
+                onClick={handlePlayer1Increment} 
+                className="btn btn-primary"
+              >+
+              </button>
+          </div>
+
+          <div className="col-xs-6">
+              <p>Player 2 {!player1Serving &&
+                <span className="pull-right label label-success">Serving</span>}
+              </p>
+              
+              <p className="well">{ player2 }</p>
+              <button 
+                onClick={handlePlayer2Increment} 
+                className="btn btn-primary"
+              >+
+              </button>
+          </div>
+      </div>
+
+      { /* winner message */}
+      <h2 className="jumbotron">Player {/* winning player here */} wins!</h2>
+
+      <hr />
+
+      { /* reset button */}
+      <button 
+        className="btn btn-danger"
+        onClick={handleReset}
+      >Reset</button>
+  </React.Fragment>
+);
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initial = {
+    player1: 0,
+    player2: 0,
+    player1Serving: true,
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const incrementPlayer1 = (state) => {
+    return { ...state, player1: state.player1 + 1 };
+};
+
+const incrementPlayer2 = (state) => {
+    return { ...state, player2: state.player2 + 1 };
+};
+    
+const server = (state) => {
+    return ((state.player1 + state.player2) % 5 === 0) ? { ...state, player1Serving: state.player1Serving = !state.player1Serving } : state;
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "incrementPlayer1": return server(incrementPlayer1(state));
+        case "incrementPlayer2": return server(incrementPlayer2(state));
+        case "reset": return initial;
+        default: return state;
+    }
+};
+
+const store = createStore(
+    reducer,
+    initial,
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+const render = () => {
+    let state = store.getState();
+    // pass in state.value as a value prop
+    ReactDOM.render(
+        <App 
+            player1={ state.player1 } 
+            player2={ state.player2 } 
+            player1Serving={ state.player1Serving }   
+            handlePlayer1Increment={ () => store.dispatch({ type: "incrementPlayer1" }) } 
+            handlePlayer2Increment={ () => store.dispatch({ type: "incrementPlayer2" }) } 
+            handleReset={ () => store.dispatch({ type: "reset" }) } 
+        />,
+        document.getElementById("root")
+    );
+};
+store.subscribe(render); // render when state changes
+render(); // render when page loads using initial state
