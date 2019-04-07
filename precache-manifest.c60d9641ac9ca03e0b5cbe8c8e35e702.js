32db343b8faee6c0900ae8cfec74a9a8a2523849
Joe Berkem <jberkem@gmail.com>
2019-04-07 12:32:55 +0100

    Updates

---
+++ precache-manifest.c60d9641ac9ca03e0b5cbe8c8e35e702.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51a49ce5c3edae1ff2aa",
+    "url": "/Ping-Pong-App-React-Redux-Practice/static/js/runtime~main.22b5a4ae.js"
+  },
+  {
+    "revision": "6b70f707aa5dc664bc66",
+    "url": "/Ping-Pong-App-React-Redux-Practice/static/js/main.94f53f53.chunk.js"
+  },
+  {
+    "revision": "fe2593acfd341e42660c",
+    "url": "/Ping-Pong-App-React-Redux-Practice/static/js/2.00f5353d.chunk.js"
+  },
+  {
+    "revision": "6b70f707aa5dc664bc66",
+    "url": "/Ping-Pong-App-React-Redux-Practice/static/css/main.56bd68a3.chunk.css"
+  },
+  {
+    "revision": "8c4a8a46b603cea1050a6dc3b4697340",
+    "url": "/Ping-Pong-App-React-Redux-Practice/index.html"
+  }
+];
